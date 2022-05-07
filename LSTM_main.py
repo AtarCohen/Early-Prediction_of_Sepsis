b6a1f02bebc81e5efa34e0a800abb78fdb6b3f7c
LiorYariv1 <lior.ya1994@gmail.com>
2022-05-07 13:33:17 +0300

    LSTM

--- LSTM_main.py
+++ LSTM_main.py
@@ -2,18 +2,20 @@ import torch
 from torch import nn
 from torch.utils.data import DataLoader
 import numpy as np
-from Trainer import Trainer
+from trainer import Trainer
 import os
 import argparse
 from datetime import datetime
-from termcolor import colored
+# from termcolor import colored
 import random
 import logging
 import itertools
-from LSTM_Model import RNN_Model, Dataset
+import pandas as pd
+from LSTM_Model import RNN_Model, Dataset, collate_inputs
 
 logger = logging.getLogger(__name__)
 ACTIVATIONS = {'relu': nn.ReLU, 'lrelu': nn.LeakyReLU, 'tanh': nn.Tanh}
+INPUT_DIM = 2
 
 
 def parsing():
@@ -30,15 +32,15 @@ def parsing():
     parser.add_argument('--time_series_model', choices=['LSTM','GRU'], default='GRU', type=str)
 
     parser.add_argument('--activation', choices=['tanh','relu','lrelu'], default='tanh', type=str)
-    parser.add_argument('--num_layers_rnn', default=4, type=int)
+    parser.add_argument('--num_layers', default=4, type=int)
     parser.add_argument('--hidden_dim', default=128, type=int)
     parser.add_argument('--bidirectional', default=False, type=bool)
-    parser.add_argument('--dropout_rnn', default=0.4, type=float)
+    parser.add_argument('--dropout', default=0.4, type=float)
 
     parser.add_argument('--eval_rate', default=1, type=int)
     parser.add_argument('--batch_size', default=4, type=int)
     parser.add_argument('--lr', default=0.05, type=float)
-    parser.add_argument('--num_epochs', default=150, type=int)
+    parser.add_argument('--num_epochs', default=1, type=int)
     args = parser.parse_args()
     assert 0 <= args.dropout <= 1
     return args
@@ -51,21 +53,25 @@ def set_seed(seed=42):
     torch.backends.cudnn.deterministic = True
 
 
-    args = parsing()
-    task_factor = [1] + 2 * [args.hands_factor]
-    set_seed()
-    logger.info(args)  # TODO : what is this?
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print('device:', device)
-    experiment_name = args.group + " task:" + args.task_str
-    logger.info(colored(experiment_name, "green"))
+args = parsing()
+set_seed()
+logger.info(args)  # TODO : what is this?
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print('device:', device)
+# logger.info(colored(experiment_name, "green"))
+full_df = pd.read_csv('/home/student/filtered_train_data_combined.csv')
+max_los = full_df[['ID','ICULOS']].groupby(by='ID').max().rename(columns={'ICULOS':'Max_ICULOS'})
+full_df=full_df.join(max_los,on='ID',how='left')
+full_df['time_bm'] =  full_df['ICULOS']-full_df['Max_ICULOS']
+full_df = full_df[full_df['time_bm']>=-10]
+full_df = full_df.fillna(-1)
+# TODO - Load DataFrames for train, validation, test and create Datasets. Train model
+ds_train = Dataset(full_df[(full_df['ID']==1)|(full_df['ID']==2)][:-2])
+dl_train = DataLoader(ds_train, batch_size=args.batch_size, collate_fn=collate_inputs, shuffle=True)
+ds_val = Dataset(full_df[(full_df['ID']==1)|(full_df['ID']==2)][:-2])
+dl_val = DataLoader(ds_val, batch_size=args.batch_size, collate_fn=collate_inputs)
+model = RNN_Model(rnn_type=args.time_series_model,input_dim = INPUT_DIM,hidden_dim = args.hidden_dim,dropout= args.dropout,num_layers =args.num_layers )
+trainer = Trainer(model)
+eval_results, train_results = trainer.train(dl_train,dl_val,args.num_epochs, args.lr,args)
 
-    # TODO - Load DataFrames for train, validation, test and create Datasets. Train model
-    ds_train = Dataset()
-    dl_train = DataLoader()
-    ds_val = Dataset()
-    dl_val = DataLoader()
-    model = create_model(args)#TODO - Implement
-    trainer = Trainer()
 
-    #TODO: Calculate Validation and test F1 and Accuracy
--- trainer.py
+++ trainer.py
@@ -1,3 +1,4 @@
+import torch
 
 from LSTM_Model import *
 import sys
@@ -5,23 +6,23 @@ from torch import optim
 import torch.nn as nn
 import math
 import pandas as pd
-from sklearn.metrics import f1_score, accuracy
+from sklearn.metrics import f1_score, accuracy_score
 # import wandb
 from datetime import datetime
 import tqdm
 
 
 class Trainer:
-    def __init__(self, model, device="cuda"):
-        self.model = model
+    def __init__(self, model, device="cpu"):
+        self.model = model.float()
         self.device = device
         self.ce = nn.CrossEntropyLoss(ignore_index=-100)
 
-    def train(self, data_loader, num_epochs, learning_rate,args,early_stop=8):
-        number_of_seqs = len(data_loader.sampler)
-        number_of_batches = len(data_loader.batch_sampler)
+    def train(self, train_data_loader,test_data_loader, num_epochs, learning_rate,args,early_stop=8,eval_rate=1):
+        number_of_seqs = len(train_data_loader.sampler)
+        number_of_batches = len(train_data_loader.batch_sampler)
         train_results_list = []
-        print(args.dataset + " " + args.group + " " + args.dataset + " dataset " + "split: " + str(test_split))
+        eval_results_list = []
 
         # wandb.init(project=args.project, group=args.group,
         #            name="split: " + str(test_split), entity=args.entity,  # ** we added entity, mode
@@ -40,32 +41,27 @@ class Trainer:
         for epoch in range(num_epochs):
             pbar = tqdm.tqdm(total=number_of_batches)
             epoch_loss = 0
-            correct1 = 0
-            total1 = 0
+            mean_epoch_acc = 0
+            mean_epoch_f1= 0
 
-            for batch in data_loader:
+            for batch in train_data_loader:
                 batch_input, batch_target, lengths, mask = batch
                 mask = mask.to(self.device)
                 optimizer.zero_grad()
                 lengths = lengths.to(dtype=torch.int64).to(device='cpu')
                 predictions = self.model(batch_input, lengths, mask)
                 loss = self.ce(predictions, batch_target)
+                epoch_loss += loss.item()
                 loss.backward()
                 optimizer.step()
-                _, predicted1 = torch.max(predictions[0], 1)
-                for i in range(len(lengths)):
-                    correct1 += (predicted1[i][:lengths[i]] == batch_target_gestures[i][
-                                                               :lengths[i]].squeeze()).float().sum().item()
-                    total1 += lengths[i]
-
-                pbar.update(1)
-            acc = accuracy(batch_target,predicted)
-            F1 =f1_score(batch_target,predicted1)
+                _, predicted = torch.max(predictions, 1)
+                acc = accuracy_score(batch_target,predicted)
+                F1 =f1_score(batch_target,predicted)
             # schedular.step(acc)
+            mean_epoch_f1 += F1
+            mean_epoch_acc += acc
             pbar.close()
-            print(colored(
-                dt_string, 'green',
-                attrs=['bold']) + f"  [epoch {epoch + 1}: train loss = {epoch_loss / number_of_seqs},   "
+            print(f"  [epoch {epoch + 1}: train loss = {epoch_loss / number_of_seqs},   "
                                   f"train acc = {acc}, train F1 = {F1}")
             train_results = {"epoch": epoch, "train loss": epoch_loss / number_of_seqs,
                              "train acc": acc, 'train F1': F1}
@@ -74,19 +70,21 @@ class Trainer:
             # wandb.log(train_results)
             train_results_list.append(train_results)
             if (epoch + 1) % eval_rate == 0:
-                print(colored("epoch: " + str(epoch + 1) + " model evaluation", 'red', attrs=['bold']))
+                print("epoch: " + str(epoch + 1) + " model evaluation")
                 results = {"epoch": epoch}
-                # results.update(self.evaluate(eval_dict, test_data_loader, list_of_vids))
-                # eval_results_list.append(results)
+                results.update(self.eval(test_data_loader))
+                eval_results_list.append(results)
+                print(f"  [epoch {epoch + 1}: train loss = {epoch_loss / number_of_seqs},   "
+                      f"train acc = {acc}, train F1 = {F1}")
 
                 # if args.upload is True:  # **controlled by wandb mode
-                wandb.log(results)
+                # wandb.log(results)
 
             # ** new:
             if F1 > best_F1 + 1e-2:
                 best_F1 = F1
                 steps_no_improve = 0
-                torch.save(self.model.state_dict(), os.path.join('/models', "F1_model.h5"))
+                torch.save(self.model.state_dict(), "F1_model.h5")
             else:
                 steps_no_improve += 1
                 if steps_no_improve >= early_stop:
@@ -94,4 +92,31 @@ class Trainer:
 
         # wandb.log({f'best_{k}': v for k, v in best_results.items()})
         # wandb.finish()
-        return train_results_list
+        return train_results_list,eval_results_list
+
+
+    def eval(self, test_data_loader):
+        results = {}
+        self.model.eval()
+        all_preds = []
+        all_labels = []
+        with torch.no_grad():
+            self.model.to(self.device)
+            for batch in test_data_loader:
+                batch_input, batch_target, lengths, mask = batch
+                mask = mask.to(self.device)
+                lengths = lengths.to(dtype=torch.int64).to(device='cpu')
+                predictions = self.model(batch_input, lengths, mask)
+                _, predicted = torch.max(predictions, 1)
+                all_preds += predicted
+                all_labels += batch_target
+            results['acc'] = accuracy_score(all_labels,all_preds)
+            results['F1'] = f1_score(all_labels,all_preds)
+        self.model.train()
+        return results
+
+
+
+
+
+
