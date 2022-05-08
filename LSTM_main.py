0be95f177b6e975ca4bfa1217730321bf873b6cb
LiorYariv1 <lior.ya1994@gmail.com>
2022-05-08 18:54:29 +0300

    LSTM updates

--- LSTM_main.py
+++ LSTM_main.py
@@ -28,9 +28,9 @@ CONST_ATTR = ['ID','max_ICULOS','Gender']
 OTHER_ATTR = ['HR','MAP','O2Sat', 'Resp','SBP','ICULOS']
 LABEL_ATTR= 'Label'
 COLS = FREQUENCY_ATTR+CONST_ATTR+LAB_ATTR+OTHER_ATTR
-TRAIN_PATH = 'filtered_train_df_0705_LSTM.csv'
-VAL_PATH = 'filtered_val_df_0705_LSTM.csv'
-TEST_PATH = 'filtered_test_df_0705_LSTM.csv'
+TRAIN_PATH = '/home/student/filtered_train_df_0705_LSTM.csv'
+VAL_PATH = '/home/student/filtered_val_df_0705_LSTM.csv'
+TEST_PATH = '/home/student/filtered_test_df_0705_LSTM.csv'
 
 
 
@@ -45,7 +45,7 @@ def parsing():
     parser.add_argument('--group', default=dt_string + " group ", type=str)
     # parser.add_argument('--use_gpu_num', default="0", type=str)
 
-    parser.add_argument('--time_series_model', choices=['LSTM','GRU'], default='GRU', type=str)
+    parser.add_argument('--time_series_model', choices=['LSTM','GRU'], default='LSTM', type=str)
 
     parser.add_argument('--num_layers', default=4, type=int)
     parser.add_argument('--hidden_dim', default=128, type=int)
@@ -53,14 +53,16 @@ def parsing():
     parser.add_argument('--dropout', default=0.4, type=float)
 
     parser.add_argument('--eval_rate', default=1, type=int)
-    parser.add_argument('--batch_size', default=4, type=int)
-    parser.add_argument('--lr', default=0.05, type=float)
-    parser.add_argument('--num_epochs', default=1, type=int)
+    parser.add_argument('--batch_size', default=32, type=int)
+    parser.add_argument('--lr', default=0.1, type=float)
+    parser.add_argument('--num_epochs', default=100, type=int)
 
     # parser.add_argument('--imputation', choices=['iterative','mean','median'], default='iterative', type=str)
     parser.add_argument('--window', default=5, type=int)
-    parser.add_argument('--seq_len', default=10, type=int)
-    parser.add_argument('--sampling', choices=['over','under','overunder'], default='overunder', type=str)
+    parser.add_argument('--seq_len', default=5, type=int)
+    parser.add_argument('--sample', choices=['over','under','overunder'], default='overunder', type=str)
+    parser.add_argument('--over_sample_rate', default='0.3', type=float)
+    parser.add_argument('--under_sample_rate', default='0.5', type=float)
 
     args = parser.parse_args()
     assert 0 <= args.dropout <= 1
@@ -90,26 +92,26 @@ def sample(df,args):
     ids = df[['ID']]
     labels = df['Label']
     if 'over' in args.sample:
-        over = RandomOverSampler(sampling_strategy=0.3)
+        over = RandomOverSampler(sampling_strategy=args.over_sample_rate)
         ids,labels = over.fit_resample(ids,labels)
     if 'under' in args.sample:
-        under = RandomUnderSampler(sampling_strategy=0.5)
+        under = RandomUnderSampler(sampling_strategy=args.under_sample_rate)
         ids,labels = under.fit_resample(ids,labels)
-    return ids
+    return ids['ID'].values
 
 args = parsing()
 set_seed()
+logger.info(args)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# TODO - Load DataFrames for train, validation, test and create Datasets. Train model
 train_df = pd.read_csv(TRAIN_PATH)
 train_patients= sample(train_df, args)
-input_dim = train_df.shape[1]-1
-ds_train = Dataset(train_patients,train_df)
+input_dim = len(COLS)
+ds_train = Dataset(train_patients,train_df,COLS)
 dl_train = DataLoader(ds_train, batch_size=args.batch_size, collate_fn=collate_inputs, shuffle=True)
 val_df = pd.read_csv(VAL_PATH)
 val_patients= list(set(val_df.ID.values))
-ds_val = Dataset(val_patients,val_df)
+ds_val = Dataset(val_patients,val_df,COLS)
 dl_val = DataLoader(ds_val, batch_size=args.batch_size, collate_fn=collate_inputs)
-model = RNN_Model(rnn_type=args.time_series_model,input_dim = input_dim,hidden_dim = args.hidden_dim,dropout= args.dropout,num_layers =args.num_layers )
+model = RNN_Model(rnn_type=args.time_series_model,bidirectional=args.bidirectional,input_dim = input_dim,hidden_dim = args.hidden_dim,dropout= args.dropout,num_layers =args.num_layers )
 trainer = Trainer(model)
 eval_results, train_results = trainer.train(dl_train,dl_val,args.num_epochs, args.lr,args)
