0268eb94c960cbd789ff947fbccd20f0e9a3963f
LiorYariv1 <lior.ya1994@gmail.com>
2022-05-08 11:50:14 +0300

    :)

--- LSTM_main.py
+++ LSTM_main.py
@@ -14,8 +14,17 @@ import pandas as pd
 from LSTM_Model import RNN_Model, Dataset, collate_inputs
 
 logger = logging.getLogger(__name__)
-ACTIVATIONS = {'relu': nn.ReLU, 'lrelu': nn.LeakyReLU, 'tanh': nn.Tanh}
-INPUT_DIM = 2
+Imputations = {}
+FREQUENCY_ATTR = [ f'{5}w_sum_{at}' for at in ['BaseExcess',  'FiO2', 'pH', 'PaCO2', 'Glucose','Lactate', 'PTT']]
+LAB_ATTR = [ 'Hct',  'Glucose','Potassium']
+CONST_ATTR = ['ID','max_ICULOS','Gender']
+OTHER_ATTR = ['time_bm','HR','MAP','O2Sat', 'Resp','SBP','ICULOS']
+LABEL_ATTR= 'Label'
+COLS = FREQUENCY_ATTR+LAB_ATTR+OTHER_ATTR
+TRAIN_PATH = 'filtered_train_df_0705.csv'
+VAL_PATH = 'filtered_val_df_0705.csv'
+TEST_PATH = 'filtered_test_df_0705.csv'
+
 
 
 def parsing():
@@ -31,7 +40,6 @@ def parsing():
 
     parser.add_argument('--time_series_model', choices=['LSTM','GRU'], default='GRU', type=str)
 
-    parser.add_argument('--activation', choices=['tanh','relu','lrelu'], default='tanh', type=str)
     parser.add_argument('--num_layers', default=4, type=int)
     parser.add_argument('--hidden_dim', default=128, type=int)
     parser.add_argument('--bidirectional', default=False, type=bool)
@@ -41,6 +49,12 @@ def parsing():
     parser.add_argument('--batch_size', default=4, type=int)
     parser.add_argument('--lr', default=0.05, type=float)
     parser.add_argument('--num_epochs', default=1, type=int)
+
+    parser.add_argument('--imputation', choices=['iterative','mean','median'], default='iterative', type=str)
+    parser.add_argument('--window', default=5, type=int)
+    parser.add_argument('--seq_len', default=10, type=int)
+    parser.add_argument('--sampling', choices=['over','under'], default='iterative', type=str)
+
     args = parser.parse_args()
     assert 0 <= args.dropout <= 1
     return args
@@ -52,13 +66,35 @@ def set_seed(seed=42):
     torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
 
+def add_rolling_window(df, attr, window_size):
+    df = df.sort_values(by=['ID','ICULOS'], ascending =[True,True])
+    rolling = df[['ID']+attr].groupby('ID').rolling(window=window_size, closed='both').count()
+    rolling= rolling.rename(columns={at: f'{window_size}w_sum_{at}' for at in attr})
+    rolling=rolling[list(rolling.columns)[1:]].reset_index().set_index('level_1')
+    combined = df.join(rolling,how='left', rsuffix= 'r')
+    return combined
+
+
+def prepare_data(df,args):
+    df = df[df['time_bm']>=-1*(args.seq_len+args.window)]
+    df = add_rolling_window(df, FREQUENCY_ATTR, args.window)
+    df = df[df['time_bm']>=-1*(args.seq_len)]
+    return df
+
+def impute_per_patient(df,args):
+
+
+def impute_and_sample(df,args):
+    if args.sampling=='under':
+
+
+
+
+
 
 args = parsing()
 set_seed()
-logger.info(args)  # TODO : what is this?
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print('device:', device)
-# logger.info(colored(experiment_name, "green"))
 full_df = pd.read_csv('/home/student/filtered_train_data_combined.csv')
 max_los = full_df[['ID','ICULOS']].groupby(by='ID').max().rename(columns={'ICULOS':'Max_ICULOS'})
 full_df=full_df.join(max_los,on='ID',how='left')
