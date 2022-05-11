8b3a37e7e0c0ac0a8796484460a44c049945c117
LiorYariv1 <lior.ya1994@gmail.com>
2022-05-11 10:19:39 +0300

    :+1:

--- Feature_Selection_Not_Serial.py
+++ Feature_Selection_Not_Serial.py
@@ -1,3 +1,5 @@
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 import pandas as pd
 import matplotlib.pyplot as plt
 plt.rc("font", size=14)
@@ -13,11 +15,19 @@ from xgboost import XGBClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import f1_score
 import pickle
+import wandb
 pd.options.mode.chained_assignment = None  # default='warn'
+pd.reset_option('all')
 
 class feature_selection():
     def __init__(self,train_path,validation_path,features=None,method = 'asc', window_size = 5, time_bm=-10,p_smote=0.5, p_under=0.5, model='RF'):
         self.frequency_used_attributes = ['BaseExcess', 'FiO2', 'pH', 'PaCO2', 'Glucose', 'Lactate', 'PTT']
+        self.freq_columns = ['BaseExcess', 'HCO3', 'FiO2', 'pH', 'PaCO2',
+                 'SaO2', 'AST', 'BUN', 'Alkalinephos', 'Calcium', 'Chloride',
+                 'Creatinine', 'Bilirubin_direct', 'Glucose', 'Lactate',
+                 'Magnesium', 'Phosphate', 'Potassium', 'Bilirubin_total',
+                 'TroponinI', 'Hct', 'Hgb', 'PTT', 'WBC', 'Fibrinogen', 'Platelets']
+        self.freq_columns_final = [f'freq_{at}' for at in self.freq_columns]
         self.values_used_attributes = ['Hct', 'Glucose', 'Potassium']
         self.constant_attributes = ['ID', 'max_ICULOS', 'Gender']
         self.other_attributes = ['time_bm', 'HR', 'MAP', 'O2Sat', 'Resp', 'SBP', 'ICULOS']
@@ -36,23 +46,28 @@ class feature_selection():
         self.feature_number = len(self.features)
         self.imputation_with_KNNimputer()
         self.X_train = pd.DataFrame(self.knn_imp.transform(self.train_df[self.features]), columns=self.features)
-        self.y_train = self.train_df['Label']
+        # self.X_train = self.train_df.fillna(0)[self.columns]
+        self.y_train = self.train_df['Label'].values
         self.val_df = self.preprocess(pd.read_csv(validation_path))
         self.X_val = pd.DataFrame(self.knn_imp.transform(self.val_df[self.features]), columns=self.features)
-        self.y_val = self.val_df['Label']
+        # self.X_val = self.val_df.fillna(0)[self.columns]
+        self.y_val = self.val_df['Label'].values
         # self.test_df = self.preprocess(pd.read_csv(test_path))
-        if model=='logreg':
-            self.model = LogisticRegression()
-        elif model=='RF':
-            self.model = RandomForestClassifier(n_estimators=100, random_state=0)
-        elif model=='XGB':
-            self.model = XGBClassifier()
         self.res = {'Train': {}, 'Val': {}}
+        self.model_name=model
         if method=='asc':
             self.chosen_features = []
-            self.feature_selection_asc()
         else:
             self.chosen_features = features
+
+    def set_model(self):
+        if self.model_name=='LR':
+            self.model = LogisticRegression()
+        elif self.model_name=='RF':
+            self.model = RandomForestClassifier(n_estimators=100, random_state=0)
+        elif self.model_name=='XGB':
+            self.model = XGBClassifier()
+
 
     def set_seed(self,seed=42):
         random.seed(seed)
@@ -66,9 +81,22 @@ class feature_selection():
         combined = df.join(rolling,how='left', rsuffix= 'r')
         return combined, rolling
 
+    def calc_frequency(self,df):
+        df = df.sort_values(by=['ID', 'ICULOS'], ascending=[True, True])
+        rolling = df[['ID','ICULOS']+self.freq_columns].groupby(by=['ID'])[self.freq_columns].expanding().count().reset_index().rename(columns={'level_1':'old_index'})
+        df=df.reset_index().rename(columns={'index':'old_index'})
+        rolling = rolling.rename(columns={at: f'freq_{at}' for at in self.freq_columns})
+        combined = pd.merge(df,rolling, on=['ID','old_index'])
+        new_cols = [f'freq_{at}' for at in self.freq_columns]
+        for at in new_cols:
+            combined[at] = combined[at] / combined['ICULOS']
+        freq_df = combined[['ID']+self.freq_columns_final].groupby(by='ID').last().reset_index()
+        return freq_df
+
 
     def preprocess(self,df):
         df, df_roll = self.add_rolling_window(df)
+        freq_df = self.calc_frequency(df)
         frequency_used_attributes_fixed = [f'{self.window_size}w_sum_{x}' for x in self.frequency_used_attributes]
         df = df[df['time_bm']>=self.time_bm]
         # handle Units123
@@ -126,7 +154,9 @@ class feature_selection():
         data_final.rename(columns={"ID__": "ID", "Label__": "Label", "max_ICULOS__":"max_ICULOS", "Gender__":"Gender"}, inplace=True)
         data_final['SOFA__max'] += data_final['SBP__median'] <= 100
         data_final['SOFA__max'] += data_final['Resp__median'] >= 22
+        data_final = pd.merge(data_final,freq_df, on='ID')
         return data_final
+
 
     def imputation_with_KNNimputer(self, n=3):
         data_knn_imputed = self.train_df[self.columns].copy(deep=True)    # Copy the data
@@ -142,6 +172,7 @@ class feature_selection():
         return os_data_X, os_data_y
 
     def feature_selection_asc(self):
+        wandb.init(project="Non_Serial_Feature_Selection", entity="labteam",mode='online',name=f'{self.model_name}_Asc Features')
         best_features = []
         best_f1 = -1
         for i in range(self.feature_number):
@@ -156,8 +187,9 @@ class feature_selection():
                 under = RandomUnderSampler(sampling_strategy=self.p_under)
                 X, y = under.fit_resample(X, y)
                 #Train model
+                self.set_model()
                 self.set_seed()
-                self.model.fit(X,y)
+                self.model.fit(X,y.values.ravel())
                 y_train_pred = self.model.predict(X)
                 res['Train'][f] = f1_score(y,y_train_pred)
                 X_val = self.X_val[cols]
@@ -170,20 +202,29 @@ class feature_selection():
             print(f'Best F1 Score for round {i+1}: {best_i_f1}')
             print(f'Feature Added: {best_i_feature}')
             self.chosen_features += [best_i_feature]
-            self.features.remove(f)
+            self.features.remove(best_i_feature)
+            wandb.log({'Epoch':i+1,'F1':best_i_f1,'Feature':best_i_feature})
             if best_i_f1 > best_f1:
                 best_f1 = best_i_f1
                 best_features = self.chosen_features
+                self.model
             self.res['Train'][i]=res['Train']
             self.res['Val'][i]=res['Val']
         self.best_features = best_features
         self.best_f1 = best_f1
         print(f'Best F1 Score: {best_f1}')
         print(f'Best Features: {best_features}')
-        with open('Results.pickle', 'wb') as handle:
+        with open(f'Results_{model}_run.pickle', 'wb') as handle:
             pickle.dump(self.res, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        with open(f'Best_features_{model}_run.pickle', 'wb') as handle:
+            pickle.dump(self.best_features, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        wandb.log({'best_features':self.best_features})
 
 
 train_path="/home/student/filtered_train_df_0705.csv"
 validation_path = "/home/student/filtered_val_df_0705.csv"
+
 F_selector = feature_selection(train_path = train_path, validation_path=validation_path)
+for model in ['RF','XGB','LR']:
+    F_selector.model_name=model
+    F_selector.feature_selection_asc()
--- LSTM_main.py
+++ LSTM_main.py
@@ -28,9 +28,9 @@ CONST_ATTR = ['ID','max_ICULOS','Gender']
 OTHER_ATTR = ['HR','MAP','O2Sat', 'Resp','SBP','ICULOS']
 LABEL_ATTR= 'Label'
 COLS = FREQUENCY_ATTR+CONST_ATTR+LAB_ATTR+OTHER_ATTR
-TRAIN_PATH = '/home/student/filtered_train_df_0705_LSTM.csv'
-VAL_PATH = '/home/student/filtered_val_df_0705_LSTM.csv'
-TEST_PATH = '/home/student/filtered_test_df_0705_LSTM.csv'
+TRAIN_PATH = '/home/student/filtered_train_df_0705_LSTM_new.csv'
+VAL_PATH = '/home/student/filtered_val_df_0705_LSTM_new.csv'
+TEST_PATH = '/home/student/filtered_test_df_0705_LSTM_new.csv'
 
 
 
@@ -76,57 +76,57 @@ def set_seed(seed=42):
     torch.backends.cudnn.deterministic = True
 
 
-def prepare_data(df,args,type):
-    df = df[df['time_bm']>=-1*(args.seq_len+args.window)]
-    df = add_rolling_window(df, FREQUENCY_ATTR, args.window)
-    df = df[df['time_bm']>=-1*(args.seq_len)]
-    if type=='train':
-        patient_ids = sample(df, args)
-    else:
-        patient_ids = list(set(df.ID.values))
-    df= df[COLS]
-    df = impute_per_patient(df,args)
-    return df, patient_ids
-
-
-class data_preperator():
-    def __init__(self,train_df,seq_len=10,window=5):
-        self.all_data_means= train_df.mean()
-        self.seq_len=seq_len
-        self.window = window
-
-    def impute_per_patient(self,df):
-        patients = list(set(df.ID.values))
-        imputed = pd.DataFrame()
-        for patient in patients:
-            tmp_df = df[df['ID']==patient][COLS+['Label']]
-            for f in LAB_ATTR+OTHER_ATTR:
-                if tmp_df[f].isnull().all:
-                    tmp_df[f]=tmp_df[f].fillna(self.all_data_means[f])
-            imp = IterativeImputer(max_iter=50, random_state=0)
-            try:
-                imp.fit(tmp_df)
-                tmp_df= pd.DataFrame(imp.transform(tmp_df), columns = COLS+['Label'])
-                imputed=imputed.append(tmp_df)
-            except:
-                print(tmp_df.shape)
-        return imputed
-
-    def add_rolling_window(self,df, attr, window_size):
-        df = df.sort_values(by=['ID','ICULOS'], ascending =[True,True])
-        rolling = df[['ID']+attr].groupby('ID').rolling(window=window_size, closed='both').count()
-        rolling= rolling.rename(columns={at: f'{window_size}w_sum_{at}' for at in attr})
-        rolling=rolling[list(rolling.columns)[1:]].reset_index().set_index('level_1')
-        combined = df.join(rolling,how='left', rsuffix= 'r')
-        return combined
-
-    def prepare_data(self,df):
-        df = df[df['time_bm']>=-1*(self.seq_len+self.window)]
-        df = self.add_rolling_window(df, frequency_used_attributes, self.window)
-        df = df[df['time_bm']>=-1*(self.seq_len)]
-        df= df[COLS+['Label']]
-        df = self.impute_per_patient(df)
-        return df
+# def prepare_data(df,args,type):
+#     df = df[df['time_bm']>=-1*(args.seq_len+args.window)]
+#     df = add_rolling_window(df, FREQUENCY_ATTR, args.window)
+#     df = df[df['time_bm']>=-1*(args.seq_len)]
+#     if type=='train':
+#         patient_ids = sample(df, args)
+#     else:
+#         patient_ids = list(set(df.ID.values))
+#     df= df[COLS]
+#     df = impute_per_patient(df,args)
+#     return df, patient_ids
+#
+#
+# class data_preperator():
+#     def __init__(self,train_df,seq_len=10,window=5):
+#         self.all_data_means= train_df.mean()
+#         self.seq_len=seq_len
+#         self.window = window
+#
+#     def impute_per_patient(self,df):
+#         patients = list(set(df.ID.values))
+#         imputed = pd.DataFrame()
+#         for patient in patients:
+#             tmp_df = df[df['ID']==patient][COLS+['Label']]
+#             for f in LAB_ATTR+OTHER_ATTR:
+#                 if tmp_df[f].isnull().all:
+#                     tmp_df[f]=tmp_df[f].fillna(self.all_data_means[f])
+#             imp = IterativeImputer(max_iter=50, random_state=0)
+#             try:
+#                 imp.fit(tmp_df)
+#                 tmp_df= pd.DataFrame(imp.transform(tmp_df), columns = COLS+['Label'])
+#                 imputed=imputed.append(tmp_df)
+#             except:
+#                 print(tmp_df.shape)
+#         return imputed
+#
+#     def add_rolling_window(self,df, attr, window_size):
+#         df = df.sort_values(by=['ID','ICULOS'], ascending =[True,True])
+#         rolling = df[['ID']+attr].groupby('ID').rolling(window=window_size, closed='both').count()
+#         rolling= rolling.rename(columns={at: f'{window_size}w_sum_{at}' for at in attr})
+#         rolling=rolling[list(rolling.columns)[1:]].reset_index().set_index('level_1')
+#         combined = df.join(rolling,how='left', rsuffix= 'r')
+#         return combined
+#
+#     def prepare_data(self,df):
+#         df = df[df['time_bm']>=-1*(self.seq_len+self.window)]
+#         df = self.add_rolling_window(df, frequency_used_attributes, self.window)
+#         df = df[df['time_bm']>=-1*(self.seq_len)]
+#         df= df[COLS+['Label']]
+#         df = self.impute_per_patient(df)
+#         return df
 
 
 
@@ -147,13 +147,16 @@ set_seed()
 logger.info(args)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 train_df = pd.read_csv(TRAIN_PATH)
+cols = list(train_df.columns)
+cols.remove('Label')
+cols.remove('ID')
 train_patients= sample(train_df, args)
-input_dim = len(COLS)
-ds_train = Dataset(train_patients,train_df,COLS)
+input_dim = len(cols)
+ds_train = Dataset(train_patients,train_df,cols)
 dl_train = DataLoader(ds_train, batch_size=args.batch_size, collate_fn=collate_inputs, shuffle=True)
 val_df = pd.read_csv(VAL_PATH)
 val_patients= list(set(val_df.ID.values))
-ds_val = Dataset(val_patients,val_df,COLS)
+ds_val = Dataset(val_patients,val_df,cols)
 dl_val = DataLoader(ds_val, batch_size=args.batch_size, collate_fn=collate_inputs)
 model = RNN_Model(rnn_type=args.time_series_model,bidirectional=args.bidirectional,input_dim = input_dim,hidden_dim = args.hidden_dim,dropout= args.dropout,num_layers =args.num_layers )
 trainer = Trainer(model)
